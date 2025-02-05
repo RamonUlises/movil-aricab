3299af05076f86ba80ff6472c01d50158d1d5c9c
RamonUlises <jorgitomilina@gmail.com>
2025-02-05 11:12:53 -0600

    Refactor Android dependencies and improve error handling in the application

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,15 @@
 import { useRouter } from "expo-router";
 import { useEffect } from "react";
 import { ActivityIndicator, View } from "react-native";
+import { LogBox, Alert } from 'react-native';
+
+LogBox.ignoreLogs(['...']);
+
+if(__DEV__) {
+  LogBox.ignoreLogs(['...']);
+} else {
+  Alert.alert("Error critico", "Ocurrio un error critico en la aplicacion, por favor contacte al administrador");
+}
 
 export default function index() {
   const router = useRouter();
